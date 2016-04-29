bcda1e194a7dcd7572d37d8a5a32abf3e8d94499
pandafulmanda <pandafulmanda@gmail.com>
2016-04-29 06:50:46 -0500

    add image field to chirp,and update model and database

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426022950) do
+ActiveRecord::Schema.define(version: 20160429114905) do
 
   create_table "chirps", force: :cascade do |t|
     t.text     "body"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "pic_file_name"
+    t.string   "pic_content_type"
+    t.integer  "pic_file_size"
+    t.datetime "pic_updated_at"
   end
 
 end
